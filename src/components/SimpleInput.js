2e1d45faa051edec1b2414e498ba0bd201889838
Ethan Ankadu <ethan.ankadu@derivco.co.za>
2023-04-23 18:39:27 +0200

    added validation to simple form and went through some of a forms validation trick using react

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,7 +1,16 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log('Name Input is valid');
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -9,15 +18,33 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = event => {
     event.preventDefault();
+    
+    setEnteredNameTouched(true);
+
+    if(enteredName.trim() == '') {
+      setEnteredNameIsValid(false)
+      return;
+    }
+
+    setEnteredNameIsValid(true);
 
     console.log(enteredName)
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setEnteredName('');
   }
 
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='form-control' onSubmit={formSubmissionHandler}>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler} />
+        <input ref={nameInputRef} type='text' id='name'value={enteredName}  onChange={nameInputChangeHandler} />
+        {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
