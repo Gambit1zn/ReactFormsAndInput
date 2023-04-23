f95d477bd4c5a43622ad4f7a0baac1c830f56917
Ethan Ankadu <ethan.ankadu@derivco.co.za>
2023-04-23 17:14:22 +0200

    added event handlers

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log(enteredName)
+  }
+
   return (
     <form>
-      <div className='form-control'>
+      <div className='form-control' onSubmit={formSubmissionHandler}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
